ec51f7b75e25d6895df79af31992ac970a0e51db
Ivanshelemekh <neo041990@mail.ru>
2019-10-13 17:14:02 +0300

    Добаавление основных сущностей в entity folder  формирование связей между ними и создание таблиц под них в db migration

--- src/main/resources/db/migration/V0.0__init.sql
+++ src/main/resources/db/migration/V0.0__init.sql
@@ -1,1 +1,36 @@
-SELECT 1;
+create table customer(
+          id long unsigneg not null auto_increment primary key ,
+          name varchar(10) not null,
+          email varchar (100) not null,
+          phone_number varchar (10) not null
+          foreign key (product_id) references product(id)
+);
+create table product(
+        id long unsigneg not null auto_increment primary key,
+        name_product varchar (100) not null, unique true ,
+        total_price double (10) not null
+          foreign key (customer_id) references customer(id)
+
+
+
+
+);
+
+create table checklist(
+        id long unsigneg not null auto_increment primary key
+        address_customer varchar (1000) not null ,
+        manager varchar (1000) not null,
+        foreign key (customer_id) references customer(id)
+
+
+);
+create table dogs_goods(
+        id long unsigneg not null auto_increment primary key,
+        name_product varchar(1000) not null,
+        quality varchar(100) not null, unique true,
+          foreign key (product_id) references product(id)
+
+
+);
+
+
