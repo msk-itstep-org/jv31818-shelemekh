36800a6fca227e7ef3fca7b2a7f4c8a16a0c0e86
Konstantin Seleznev <kkseleznev@avito.ru>
2019-11-26 17:40:31 +0300

    Fixes

--- src/main/java/org/itstep/msk/app/controller/MainController.java
+++ src/main/java/org/itstep/msk/app/controller/MainController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Controller
-public class Maincontroller {
+public class MainController {
 
     @GetMapping("/")
     public String getIndex(){
