b4cb46eca5f4f5579eed0094e40fca825cd10008
Ivanshelemeh <neo041990@mail.ru>
2021-08-27 18:09:13 +0300

    fill interface with method getBySpecific

--- src/main/java/org/itstep/msk/app/repository/CartRepository.java
+++ src/main/java/org/itstep/msk/app/repository/CartRepository.java
@@ -1,7 +1,11 @@
 package org.itstep.msk.app.repository;
 
 import org.itstep.msk.app.entity.Cart;
+import org.itstep.msk.app.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CartRepository extends JpaRepository<Cart,Integer> {
+public interface CartRepository extends JpaRepository<Cart,Integer> {g
+    Product getBySpecifTitle(@Param("totalprice") Double price);
 }
