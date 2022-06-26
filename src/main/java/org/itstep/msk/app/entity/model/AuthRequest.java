ba8d73ce7dd37e9b00edcc0918a698f529b2a2f4
Ivanshelemeh <neo041990@mail.ru>
2022-06-26 15:25:59 +0300

    feat: add Testcontainers feature to enable to run test customer repository
    BREAKING CHANGE use others features of Testcontainers mode

--- src/main/java/org/itstep/msk/app/entity/model/AuthRequest.java
+++ src/main/java/org/itstep/msk/app/entity/model/AuthRequest.java
@@ -1,33 +1,16 @@
 package org.itstep.msk.app.entity.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * DTO model for auth request
  */
+@Data
+@AllArgsConstructor
+@Builder
 public class AuthRequest {
-
-    public AuthRequest() {
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getCustomerPassword() {
-        return customerPassword;
-    }
-
-    public void setCustomerPassword(String customerPassword) {
-        this.customerPassword = customerPassword;
-    }
-
-    public AuthRequest(String customerName, String customerPassword) {
-        this.customerName = customerName;
-        this.customerPassword = customerPassword;
-    }
 
     private String customerName;
     private String customerPassword;
--- src/test/java/org/itstep/msk/app/repotests/CustomerRepoTest.java
+++ src/test/java/org/itstep/msk/app/repotests/CustomerRepoTest.java
@@ -3,43 +3,68 @@ package org.itstep.msk.app.repotests;
 
 import org.itstep.msk.app.entity.Customer;
 import org.itstep.msk.app.repository.CustomerRepository;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
+
+
 @DataJpaTest
 @Transactional
-@TestPropertySource("/application-test.properties")
-@ExtendWith(SpringExtension.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@TestPropertySource("/application-test.properties")
+//@ExtendWith(SpringExtension.class)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+@Testcontainers
 public class CustomerRepoTest {
 
     @Autowired
     private CustomerRepository customRepository;
 
-    @Autowired
-    private TestEntityManager testEntityManager;
-
-
     private Customer customer;
 
+    @Container
+    private static MySQLContainer container = (MySQLContainer) new MySQLContainer("mysql:8.0.26")
+            .withStartupCheckStrategy(
+                    new OneShotStartupCheckStrategy().withTimeout(Duration.ofSeconds(30))
+            );
+
+
+    @DynamicPropertySource
+    private static void initProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.driver-class-name",container::getDriverClassName);
+    }
+
+    @Test
+    void getContainerIp(){
+        assertThat(container.getContainerId()).isNotNull();
+    }
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         customRepository.deleteAll();
-        customer = new Customer("12344ert","Ivan","zreo041990@gmai.com");
+        customer = new Customer("12344ert", "Ivan", "zreo041990@gmai.com");
         customer.setId(2);
         customer.setPhoneNumber("+38745699934");
         customRepository.save(customer);
@@ -47,25 +72,26 @@ public class CustomerRepoTest {
 
 
     @Test
-    @Sql("/customer_test.sql")
-    public void testShouldWorkWell(){
-        Customer saveCustomer = testEntityManager.persistAndFlush(customRepository.findByName("Ivan"));
-        assertThat(customer.getName()).isEqualTo(saveCustomer.getName());
+    //  @Sql("/customer_test.sql")
+    public void testShouldWorkWell() {
+        Customer saveCustomer = customRepository.findByName("Ivan");
+        assertThat(saveCustomer.getName()).isNotEmpty();
 
     }
+
     @Test
-    @Sql("/customer_test.sql")
-    public void updateTest(){
+    // @Sql("/customer_test.sql")
+    public void updateTest() {
         customer.setName("Mike");
         customRepository.save(customer);
 
-        var foundCustomer= customRepository.findByName(customer.getName());
-        assertEquals("Mike",foundCustomer.getName());
+        var foundCustomer = customRepository.findByName(customer.getName());
+        assertEquals("Mike", foundCustomer.getName());
     }
 
     @Test
-    @Sql("/customer_test.sql")
-    public void deleteTest(){
+    //  @Sql("/customer_test.sql")
+    public void deleteTest() {
         customRepository.delete(customer);
         assertFalse(customRepository.existsById(customer.getId()));
     }
